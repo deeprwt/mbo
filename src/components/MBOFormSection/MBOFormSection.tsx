6ef515147ee6726833baee95abd703b713220b17
Deepak Rawat <rwtroshan28@gmail.com>
2025-05-19 13:09:06 +0530

    comment on the react import

--- src/components/MBOFormSection/MBOFormSection.tsx
+++ src/components/MBOFormSection/MBOFormSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Box, Typography, TextField } from "@mui/material";
 import { MBOFileAttachment } from "@/components/MBOFileAttachment";
 
